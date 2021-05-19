b94acf2769b2572b5bd5dae8ecacea79f013950e
aayushsaini <aayush.r98@gmail.com>
2021-05-19 11:04:20 +0530

    Graph implementation -1 py

--- DS/graph.py
+++ DS/graph.py
@@ -1,8 +1,31 @@
+class Graph:
+    def __init__(self, edges):
+        self.edges = edges
+        self.graph_dict = {}
+        for start, end in self.edges:
+            if start in self.graph_dict:
+                self.graph_dict[start].append(end)
+            else:
+                self.graph_dict[start] = [end]
+        print("Graph generated: ", self.graph_dict)
 
+    def get_path(self, start, end, path=[]):
+        path = path + [start]
+        paths = []
+        if (start == end):
+            return path
+        elif (start not in self.graph_dict[start]):
+            return None
+        for node in self.edges:
+            if node not in self.graph_dict[start]:
 
 if __name__ == '__main__':
     routes = [["Mumbai", "Paris"],
               ["Mumbai", "Dubai"],
               ["Paris", "Dubai"],
               ["Paris", "New York"],
-              
+              ["Dubai", "New York"],
+              ["New York", "Toronto"]]
+
+    routes_graph = Graph(routes)
+
